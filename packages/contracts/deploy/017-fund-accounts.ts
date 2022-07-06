8e290aa07b0c489545ba5311ab9a927e1d6d23e0
Theo Lee <na4980@gmail.com>
2022-07-06 16:08:17 +0900

    [OR-132] Add L1 BOBA & Boba_GasPriceOracle contract

--- packages/contracts/deploy/017-fund-accounts.ts
+++ packages/contracts/deploy/017-fund-accounts.ts
@@ -4,9 +4,13 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 import { defaultHardhatNetworkHdAccountsConfigParams } from 'hardhat/internal/core/config/default-config'
 import { normalizeHardhatNetworkAccountsConfig } from 'hardhat/internal/core/providers/util'
 
+// eslint-disable-next-line import/order
+import { predeploys } from '../src/predeploys'
+
 /* Imports: Internal */
 import { getContractFromArtifact, isHardhatNode } from '../src/deploy-utils'
 import { names } from '../src/address-names'
+import { getDeployedContract } from '../src/hardhat-deploy-ethers'
 
 // This is a TEMPORARY way to fund the default hardhat accounts on L2. The better way to do this is
 // to make a modification to hardhat-ovm. However, I don't have the time right now to figure the
@@ -22,13 +26,25 @@ const deployFn: DeployFunction = async (hre) => {
       }
     )
 
+    const L1BobaToken = await getDeployedContract(hre, 'TK_L1BOBA', {
+      iface: 'BOBA',
+    })
+
     // Default has 20 accounts but we restrict to 20 accounts manually as well just to prevent
     // future problems if the number of default accounts increases for whatever reason.
     const accounts = normalizeHardhatNetworkAccountsConfig(
       defaultHardhatNetworkHdAccountsConfigParams
     ).slice(0, 20)
 
+    // BOBA_TEMPORARY: Boba holder
+    const BobaHolder = new hre.ethers.Wallet(
+      accounts[0].privateKey,
+      hre.ethers.provider
+    )
+
     // Fund the accounts in parallel to speed things up.
+
+    // TODO: fund BOBA to L2 accounts
     await Promise.all(
       accounts.map(async (account, index) => {
         // Add a sleep here to avoid any potential issues with spamming hardhat. Not sure if this
@@ -50,6 +66,27 @@ const deployFn: DeployFunction = async (hre) => {
             depositAmount
           )} ETH`
         )
+        // BOBA_TEMPORARY: Deposit Boba tokens to L2 accounts
+        const depositBobaAmount = hre.ethers.utils.parseEther('5000')
+        const L2BobaAddress = predeploys.L2StandardERC20
+        const approveTx = await L1BobaToken.connect(BobaHolder).approve(
+          L1StandardBridge.address,
+          depositBobaAmount
+        )
+        await approveTx.wait()
+        const fundBobaTx = await L1StandardBridge.connect(
+          BobaHolder
+        ).depositERC20To(
+          L1BobaToken.address,
+          L2BobaAddress,
+          wallet.address,
+          depositBobaAmount,
+          8_000_000,
+          '0x',
+          { gasLimit: 2_000_000 } // Idk, gas estimation was broken and this fixes it.
+        )
+        await fundBobaTx.wait()
+        console.log(`✓ Funded ${wallet.address} on L2 with 5000.0 BOBA`)
       })
     )
   }
