c41d7f665dd53cea1fa66655d188959272ada0c9
Theo Lee <na4980@gmail.com>
2022-07-15 17:08:36 +0900

    [OR-172] Add TOKAMAK contract and update @openzeppelin/contracts library

---
+++ packages/contracts/contracts/L1/token/MinterRole.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import { Context } from "@openzeppelin/contracts/utils/Context.sol";
+import { Roles } from "./Roles.sol";
+
+contract MinterRole is Context {
+    using Roles for Roles.Role;
+
+    event MinterAdded(address indexed account);
+    event MinterRemoved(address indexed account);
+
+    Roles.Role private _minters;
+
+    // Non-abstract contracts cannot have internal constructors
+    constructor() {
+        _addMinter(_msgSender());
+    }
+
+    modifier onlyMinter() {
+        require(isMinter(_msgSender()), "MinterRole: caller does not have the Minter role");
+        _;
+    }
+
+    function isMinter(address account) public view returns (bool) {
+        return _minters.has(account);
+    }
+
+    function addMinter(address account) public onlyMinter {
+        _addMinter(account);
+    }
+
+    function renounceMinter() public {
+        _removeMinter(_msgSender());
+    }
+
+    function _addMinter(address account) internal {
+        _minters.add(account);
+        emit MinterAdded(account);
+    }
+
+    function _removeMinter(address account) internal {
+        _minters.remove(account);
+        emit MinterRemoved(account);
+    }
+}
--- packages/contracts/contracts/L1/token/TOKAMAK.sol
+++ packages/contracts/contracts/L1/token/TOKAMAK.sol
@@ -1,39 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// access control
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+// have permission to mint (create) new tokens as they see fit.
+// now it is not used.
+import { ERC20Mintable } from "./ERC20Mintable.sol";
 
 /**
- * @title Tokamak Token (TOKAMAK)
- *
+ * @dev Current implementations is just for testing seigniorage manager.
  */
-
-contract TOKAMAK is Context, ERC20 {
-    /// @notice Maximum possible number of tokens
-    uint224 public constant maxSupply = 500000000e18; // 500 million TOKAMAK
-
-    function _maxSupply() internal pure returns (uint224) {
-        return maxSupply;
-    }
-
-    constructor() ERC20("Tokamak Token", "TOKAMAK") {
-        //mint maxSupply at genesis, allocated to deployer
-        _mint(_msgSender(), maxSupply);
-    }
-
-    function _mint(address _to, uint256 _amount) internal override(ERC20) {
-        super._mint(_to, _amount);
-    }
-
-    function _burn(address _account, uint256 _amount) internal override(ERC20) {
-        super._burn(_account, _amount);
-    }
-
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20) {
-        super._afterTokenTransfer(from, to, amount);
-    }
+contract TOKAMAK is ERC20, Ownable, ERC20Mintable {
+    constructor() ERC20("Tokamak Test Token", "TOKAMAK") {}
 }
