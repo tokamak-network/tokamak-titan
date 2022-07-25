946e13de0173267bad997f49c8fe8d669423fb37
Harry Oh <harry@5004.pe.kr>
2022-07-25 21:06:09 +0900

    [OR-178] Add scripts for generating addresses files

---
+++ packages/contracts/scripts/generate-state-dump.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+get_abs_filename() {
+  echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+}
+
+MYPATH="$(cd "$(dirname "$0")"; pwd -P)"
+ENVFILE=$(get_abs_filename $MYPATH/../.env)
+[ -e $ENVFILE ] && export $(cat .env | sed 's/#.*//g' | xargs)
+
+if [ -z "$CONTRACTS_TARGET_NETWORK" ]; then
+    echo "Must specify \$CONTRACTS_TARGET_NETWORK."
+    exit 1
+fi
+
+OUTPUT=$(get_abs_filename $MYPATH/../genesis/$CONTRACTS_TARGET_NETWORK)
+OUTPUT_FILE=$OUTPUT.json
+STATE_DUMP_FILE=$OUTPUT-state-dump.latest.json
+npx hardhat take-dump --network $CONTRACTS_TARGET_NETWORK
+
+mv $OUTPUT_FILE $STATE_DUMP_FILE
+
+echo "Generated state dump file($STATE_DUMP_FILE)"
