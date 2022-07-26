052655f4012899a80dcf9402baa8ebe3945a77df
Harry Oh <harry@5004.pe.kr>
2022-07-26 16:03:49 +0900

    [OR-178] Add hardhat-remote testnet for development

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -52,6 +52,12 @@ const config: HardhatUserConfig = {
       url: 'http://127.0.0.1:8545',
       accounts: [privateKey],
     },
+    'hardhat-remote': {
+      chainId: 31337,
+      url: process.env.CONTRACTS_RPC_URL || '',
+      deploy,
+      accounts: [privateKey],
+    },
     rinkeby: {
       chainId: 4,
       url: process.env.CONTRACTS_RPC_URL || '',
