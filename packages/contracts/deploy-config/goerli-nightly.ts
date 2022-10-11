3e13b3c3ec36d7113e931b2f0db4c34bf61838dd
Harry Oh <harry@5004.pe.kr>
2022-10-11 15:51:30 +0900

    [OR-331] redeploy contract to goerli-nightly

--- packages/contracts/deploy-config/goerli-nightly.ts
+++ packages/contracts/deploy-config/goerli-nightly.ts
@@ -9,7 +9,7 @@ const config = {
   sccSequencerPublishWindowSeconds: 12592000,
   ovmSequencerAddress: '0x60F8A2680b67db4E2313762B100391051cB403E0',
   ovmProposerAddress: '0x1c7c7cC2ab971Be3E6a2c7Bddb7392DB3CE2e87b',
-  ovmBlockSignerAddress: '0x00000398232E2064F896018496b4b44b3D62751F',
+  ovmBlockSignerAddress: '0xa159967552001d881F01Da6E6E2e4754968e5905',
   ovmFeeWalletAddress: '0x11Bc07A192bf1E0B8D46458DFdEeDC02E021C990',
   ovmAddressManagerOwner: '0xff5123d22088ecbf74C8A56E1F538bECDcaCF544',
   ovmGasPriceOracleOwner: '0xff49D1cFBb66624bEb16d1a74F8ac46dA76aFcC7',
