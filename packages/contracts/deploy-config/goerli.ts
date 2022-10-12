99a7f0e1ff441743acf908489a8b67e79a2049a4
Harry Oh <harry@5004.pe.kr>
2022-10-12 22:10:48 +0900

    [OR-331] redeploy contract for goerli testnet

--- packages/contracts/deploy-config/goerli.ts
+++ packages/contracts/deploy-config/goerli.ts
@@ -9,7 +9,7 @@ const config = {
   sccSequencerPublishWindowSeconds: 12592000,
   ovmSequencerAddress: '0x28bbaEE27683e1C252dDa05c4C43FF5E73A702be',
   ovmProposerAddress: '0x6Bcb3B9ee42BCbe3C67ad3e6D28Ca5a258677E98',
-  ovmBlockSignerAddress: '0x00000398232E2064F896018496b4b44b3D62751F',
+  ovmBlockSignerAddress: '0x85ffE3176131188E265Cc82869555cd78f394224',
   ovmFeeWalletAddress: '0x091C8a37384ED31a469Ff23Fe4A183D11f56B22b',
   ovmAddressManagerOwner: '0x37212a8F2abbb40000e974DA82D410DdbecFa956',
   ovmGasPriceOracleOwner: '0x8F3E9A5c4Ee4092E8CF3159dc65090CFD7e63D2e',
