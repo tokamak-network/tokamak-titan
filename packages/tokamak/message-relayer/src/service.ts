bdc8d46fa7c5efbff0aefc51ae6139b4f501375d
Theo Lee <theo.l@onther.io>
2023-01-18 14:53:44 +0900

    [OR-464] get filter

--- packages/tokamak/message-relayer/src/service.ts
+++ packages/tokamak/message-relayer/src/service.ts
@@ -16,7 +16,6 @@ import {
   CHAIN_BLOCK_TIMES,
 } from '@eth-optimism/sdk'
 import { Provider } from '@ethersproject/abstract-provider'
-
 import 'dotenv/config'
 
 type MessageRelayerOptions = {
@@ -244,7 +243,9 @@ export class MessageRelayerService extends BaseServiceV2<
     this.state.relayerFilter = []
     this.state.fastRelayerFilter = []
     this.state.lastFilterPollingTimestamp = 0
-
+    if (process.env.TOKAMAK_CONTRACTS_URL) {
+      this.options.filterEndpoint = process.env.TOKAMAK_CONTRACTS_URL
+    }
     //batch system
     this.state.timeOfLastRelayS = Date.now()
     this.state.messageBuffer = []
@@ -266,6 +267,9 @@ export class MessageRelayerService extends BaseServiceV2<
       await sleep(1000)
       return
     }
+
+    // get filter
+    await this._getFilter()
 
     this.logger.info(`checking L2 block ${this.state.highestCheckedL2Tx}`)
 
@@ -335,6 +339,50 @@ export class MessageRelayerService extends BaseServiceV2<
 
     // All messages have been relayed so we can move on to the next block.
     this.state.highestCheckedL2Tx++
+  }
+
+  private async _getFilter(): Promise<void> {
+    this.state.lastFilterPollingTimestamp = 0
+    const right =
+      this.state.lastFilterPollingTimestamp + this.options.filterPollingInterval
+    console.log(
+      this.state.lastFilterPollingTimestamp,
+      this.options.filterPollingInterval,
+      new Date().getTime() > right
+    )
+    try {
+      // fast_relayer
+      if (this.options.filterEndpoint) {
+        if (
+          this.state.lastFilterPollingTimestamp === 0 ||
+          new Date().getTime() >
+            this.state.lastFilterPollingTimestamp +
+              this.options.filterPollingInterval
+        ) {
+          const addressManager = getContractFactory('Lib_AddressManager')
+            .connect(this.state.wallet)
+            .attach(this.options.addressManagerAddress)
+          const L1LiquidityPool = await addressManager.getAddress(
+            'Proxy__L1LiquidityPool'
+          )
+          const L1StandardBridge = await addressManager.getAddress(
+            'Proxy__OVM_L1StandardBridge'
+          )
+          const fastRelayerFilterSelect = [L1LiquidityPool]
+          const relayerFilterSelect = [L1StandardBridge]
+
+          this.state.lastFilterPollingTimestamp = new Date().getTime()
+          this.state.fastRelayerFilter = fastRelayerFilterSelect
+          this.state.relayerFilter = relayerFilterSelect
+          this.logger.info('Found the two filters', {
+            relayerFilterSelect,
+            fastRelayerFilterSelect,
+          })
+        }
+      }
+    } catch {
+      this.logger.error('CRITICAL ERROR: Failed to fetch the Filter')
+    }
   }
 }
 
