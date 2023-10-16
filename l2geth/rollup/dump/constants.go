85676228abccba9c2a51f81ac4768e8f19dbe784
Theo <theo.l@onther.io>
2023-10-16 02:48:43 +0000

    add config to run standalone l2geth in goerli-nightly & add OvmTonAddress

--- l2geth/rollup/dump/constants.go
+++ l2geth/rollup/dump/constants.go
@@ -5,5 +5,6 @@ import (
 )
 
 var OvmEthAddress = common.HexToAddress("0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000")
+var OvmTonAddress = common.HexToAddress("0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2")
 var OvmFeeWallet = common.HexToAddress("0x4200000000000000000000000000000000000011")
 var OvmWhitelistAddress = common.HexToAddress("0x4200000000000000000000000000000000000002")
