0fea6c32ee4a60eed532be17b24ea7177de38b28
Theo <theo.l@onther.io>
2023-11-20 09:55:34 +0000

    avoid deploying l1 & permit only 55004, 5050, 5051

--- packages/tokamak/contracts/deploy/019-Tokamak-TONFeeVault.deploy.ts
+++ packages/tokamak/contracts/deploy/019-Tokamak-TONFeeVault.deploy.ts
@@ -4,9 +4,11 @@ import { DeployFunction } from 'hardhat-deploy/dist/types'
 /* Imports: Internal */
 import { deployAndVerifyAndThen, isHardhatNode } from '../src/deploy-utils'
 
+const liveNetworks: number[] = [55004, 5050, 5051]
+
 const deployFn: DeployFunction = async (hre) => {
   // Only live network
-  if (!await isHardhatNode(hre)) {
+  if (!await isHardhatNode(hre) && liveNetworks.includes(hre.network.config.chainId)) {
     console.log('deploy TON_FeeVault')
     await deployAndVerifyAndThen({
       hre,
