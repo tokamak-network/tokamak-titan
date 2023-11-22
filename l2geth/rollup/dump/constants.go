2a1d3c146d2d1aaaad13b500b36773ee246b5c31
Theo <theo.l@onther.io>
2023-11-22 10:28:20 +0000

    define TON contract addresses

--- l2geth/rollup/dump/constants.go
+++ l2geth/rollup/dump/constants.go
@@ -7,3 +7,8 @@ import (
 var OvmEthAddress = common.HexToAddress("0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000")
 var OvmFeeWallet = common.HexToAddress("0x4200000000000000000000000000000000000011")
 var OvmWhitelistAddress = common.HexToAddress("0x4200000000000000000000000000000000000002")
+
+// TODO: unify after holesky testnet update
+var titanTonAddress = common.HexToAddress("0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2")
+var titanGoerliTonAddress = common.HexToAddress("0xFa956eB0c4b3E692aD5a6B2f08170aDE55999ACa")
+var titanGoerliNightlyTonAddress = common.HexToAddress("0x7c6b91D9Be155A6Db01f749217d76fF02A7227F2")
